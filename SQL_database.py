4427209e103c06ff70119a34671ff2c24ab9379d
Garrosh888 <hallaseferovaziz0826@gmail.com>
2024-05-18 14:58:18 +0300

    подключина база данных к  основному модолю

--- SQL_database.py
+++ SQL_database.py
@@ -1,25 +1,32 @@
 import sqlite3
-def delete_password(id):
-    cursor.execute("DELETE FROM table_password WHERE id = ?",(id,))
-    connect.commit()
-def printer():
-    cursor.execute("SELECT * FROM table_password")
-    passwords = cursor.fetchall()
-    for i in passwords:
-        print(i)
-def insert_data(password,description,date):
-    cursor.execute("INSERT INTO table_password (password,description,date) VALUES (?, ?, ?)",(password,description,date))
-    connect.commit()
-def create_table():
-    cursor.execute("CREATE TABLE IF NOT EXISTS table_password(id INTEGER PRIMARY KEY,password TEXT,description TEXT,date TEXT )")
-connect = sqlite3.connect("data_base_password.db")#execute - отправка запроса в базу данных
+class save_sql_table():
+    def __init__(self):
+        self.connect = sqlite3.connect("data_base_password.db")  # execute - отправка запроса в базу данных
+        self.cursor_object = self.connect.cursor()  # обьект для отправки запросов в базу данных
 
-cursor = connect.cursor()#обьект для отправки запросов в базу данных
 
-create_table()
-#insert_data("12345","hello","14.05.2024")
-#insert_data("56789","good bye","14.05.2024")
-#insert_data("11121","hello","14.05.2024")
-#delete_password(2)
-printer()
-connect.close()
+    def edit_table(self,id,new_password,new_description):
+        self.cursor_object.execute(f"UPDATE table_password SET password = ? ,description = ? WHERE id = ?",(new_password,new_description,id))
+        self.connect.commit()
+    def delete_password(self,id):
+        self.cursor_object.execute("DELETE FROM table_password WHERE id = ?",(id,))
+        self.connect.commit()
+    def printer(self):
+        self.cursor_object.execute("SELECT * FROM table_password")
+        passwords = self.cursor_object.fetchall()
+        for i in passwords:
+            print(i)
+    def insert_data(self,password,description,date):
+        self.cursor_object.execute("INSERT INTO table_password (password,description,date) VALUES (?, ?, ?)",(password,description,date))
+        self.connect.commit()
+    def create_table(self):
+        self.cursor_object.execute("CREATE TABLE IF NOT EXISTS table_password(id INTEGER PRIMARY KEY,password TEXT,description TEXT,date TEXT )")
+
+    def disconnect_data_base(self):
+        self.connect.close()
+    def get_info(self,save_passwords):
+        pass
+
+#object_save_sql_table = save_sql_table()
+#object_save_sql_table.printer()
+#object_save_sql_table.disconnect_data_base()
